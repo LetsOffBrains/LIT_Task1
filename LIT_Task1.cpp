4652cc98e983eefeb01823276ee42cda8786f7b1
LetsOffBrains <darkhorseaka@ya.ru>
2023-02-14 17:54:18 +0700

    Initial commit

---
+++ LIT_Task1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+    unsigned int n = 30;
+
+    std::cout << "Count up to: " << std::endl;
+    std::cin >> n;
+
+    unsigned long f = 1;
+    for (unsigned i = 2; i <= n; f *= i++) {
+        if ((f + 1) % i != 0) {
+            std::cout << i << " ";
+        }
+    }
+
+    std::cout << std::endl;
+}
